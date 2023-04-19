3e0c36406987293151a2808cbf750a00a8b40e61
johnportin <jportin13@gmail.com>
2023-04-19 13:44:49 -0500

    Move non-og meta tags to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>John Portin</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import ProfilePicture from "../images/profile.jpg";
 
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head>
-        <title>John Portin</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="John Portin" />
         <meta property="og:description" content="John Portin's Portfolio." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://johnportin.me" />
-        <meta property="og:image" content={ProfilePicture} />
+        <meta
+          property="og:image"
+          content="https://johnportin.me/images/profile.jpg"
+        />
         <meta property="og:image:width" content="512" />
         <meta property="og:image:height" content="512" />
       </Head>
