f9a4012f66730522401a61c6da0493133d45b1a6
johnportin <jportin13@gmail.com>
2023-04-19 11:32:11 -0500

    Update og properties

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <title>John Portin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="John Portin" />
+        <meta property="og:description" content="John Portin's Portfolio." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://johnportin.me" />
+        <meta property="og:image" content="../images/profile.jpg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
