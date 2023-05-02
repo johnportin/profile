81078c59ed7e7b8fd283f73adaef7e7596a063a3
johnportin <jportin13@gmail.com>
2023-05-02 15:38:10 -0500

    Fix Card component target attribute

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card({ title, image, content, href }) {
   return (
     <div className="flex flex-col md:self-start md:items-start md:justify-start w-full gap-4">
       <h1 className="text-3xl  ">{title}</h1>
-      <a href={href} className="">
+      <a href={href} target="_blank" className="">
         <Image
           src={image}
           className="rounded-2xl object-none hover:shadow-lg hover:shadow-slate-500 duration-200"
