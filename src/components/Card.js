99b21da06c460486024778c7d6bcc1b2d052f3c1
johnportin <jportin13@gmail.com>
2023-05-02 16:02:08 -0500

    Add new project photos. Update styling to accommodate

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,13 +6,15 @@ export default function Card({ title, image, content, href }) {
   return (
     <div className="flex flex-col md:self-start md:items-start md:justify-start w-full gap-4">
       <h1 className="text-3xl  ">{title}</h1>
-      <a href={href} target="_blank" className="">
-        <Image
-          src={image}
-          className="rounded-2xl object-none hover:shadow-lg hover:shadow-slate-500 duration-200"
-          alt="Project Image"
-        />
-      </a>
+      <div className="md:w-full h-60 overflow-hidden rounded-2xl">
+        <a href={href} target="_blank" className="">
+          <Image
+            src={image}
+            className="block h-full w-full object-cover object-center hover:shadow-lg hover:shadow-slate-500 duration-200"
+            alt="Project Image"
+          />
+        </a>
+      </div>
       <p>{content}</p>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,15 @@
 import Card from "./Card";
 import { lorem } from "../utils/utils";
 import Picture from "../images/project-placeholder.jpg";
+import BattleShip from "../images/battleship.png";
+import Cian from "../images/cian.png";
+import LittleLemon from "../images/littleLemon.png";
+import Roster from "../images/roster.png";
 
 const projects = [
   {
     title: "Digital Roster",
-    image: Picture,
+    image: Roster,
     content: `Keeping track of student attendance can be a chore for any teacher. 
         This app allows teachers to track their students attendance -- and more -- in a digital format. 
         Teachers can easily manage multiple classes of students with the ability to:
@@ -17,7 +21,7 @@ const projects = [
   },
   {
     title: "Wellness Tracker",
-    image: Picture,
+    image: Cian,
     content: `
         This app allows users to track their mood.
         Users can add new mood entries by taking a short quiz.
@@ -27,7 +31,7 @@ const projects = [
   },
   {
     title: "Meta Front-End Capstone",
-    image: Picture,
+    image: LittleLemon,
     content: `This website is the capstone project of Meta's Front-End Developer Course on Coursera.
     This gave me the opportunity to build a website from scratch.
     Starting from wireframes in figma, adhering to the Nielsen Heuristics, and finally producing a fully functional website with React.`,
@@ -35,7 +39,7 @@ const projects = [
   },
   {
     title: "BattleShip",
-    image: Picture,
+    image: BattleShip,
     content: `This is the classic game of battleship. Together with a group of fellow students, we worked together to build this game.
         We built the entire thing with basic web technologies: HTML, CSS, and JavaScript.
         It was a lot of fun to work together with others to build something from scratch!`,
